4824e2dcd2e65e9d1bad934aecdb1f9ca5c2d008
Your Name <you@example.com>
2025-06-03 07:56:57 -0700

    Pass all tests: add divide, square, and add functions

---
+++ index.js
@@ -0,0 +1,14 @@
+const myFunction = function() {
+  return "Hello!";
+};
+
+const add = (a, b) => a + b;
+
+const nums = [1, 2, 3];
+const doubled = nums.map(n => n * 2);
+
+const divide = function() {
+  return 2000 / 100;
+};
+
+const square = x => x * x;
